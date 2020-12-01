5e20717191420cedc5fb72f6f151e233c751c2e6
kshitish <kshitish182@gmail.com>
2020-12-01 20:38:28 +0545

    Typescript initialization

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,2 @@
 "use strict";
-var count = 2;
-var promise = new Promise(function (res, rej) {
-    if (count == 2) {
-        res();
-    }
-    else {
-        rej();
-    }
-});
-promise.then(function () { return console.log("It is resoleved"); }).catch(function () { return console.log("Rejected"); });
+console.log("hello worlds");
