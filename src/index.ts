1d4aec8e48e7e45805f134fe7ebc1f6f856f46be
kshitish <kshitish182@gmail.com>
2021-01-05 13:24:37 +0545

    Minor change

--- src/index.ts
+++ src/index.ts
@@ -49,4 +49,6 @@ async function connectDb(req: Request, res: Response, next: NextFunction) {
   }
 }
 
-app.listen(5000, () => console.log('Listening to port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
